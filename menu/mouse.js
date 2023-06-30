03a50bc53281f0a446e957744cd5db2c9eb7abb1
lebronfruit <77854576+lebronfruit@users.noreply.github.com>
2023-06-30 15:34:15 -0700

    Delete menu directory

--- menu/mouse.js
+++
@@ -1,9 +0,0 @@
-class mouse {
-    static x = 0
-    static y = 0
-
-    static update(event) {
-        mouse.x = Screen.translateX(event.clientX)
-        mouse.y = Screen.translateY(event.clientY)
-    }
-}
